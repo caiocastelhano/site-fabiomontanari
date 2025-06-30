1875cfd29e5b0ae778d856c1a991d35557f7cc68
Caio Castelhano <castelhanoc@gmail.com>
2025-06-30 11:57:35 -0300

    Adiciona galeria e modal na página Industry

--- src/app/industry-research/page.jsx
+++ src/app/industry-research/page.jsx
@@ -1,10 +1,11 @@
-"use client";
+'use client';
 
 import styles from './page.module.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useLanguage } from '../context/LanguageContext';
 import industryDictionary from './industryDictionary';
+import IndustryGallery from './IndustryGallery';
 
 export default function IndustryResearchPage() {
   const { language } = useLanguage();
@@ -37,16 +38,7 @@ export default function IndustryResearchPage() {
           <h1 className={styles.title}>Industry Engagement & Research</h1>
           <p className={styles.intro}>{t.intro}</p>
 
-          {images.map(({ filename, key }) => (
-            <figure key={key} className={styles.figure}>
-              <img
-                src={`/images/industry-research/${filename}`}
-                alt={t.captions[key]}
-                className={styles.image}
-              />
-              <figcaption className={styles.caption}>{t.captions[key]}</figcaption>
-            </figure>
-          ))}
+          <IndustryGallery images={images} captions={t.captions} />
         </div>
       </main>
 
