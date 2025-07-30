e89242df703f51ff3cd466878293c0b993b247a1
Caio Castelhano <castelhanoc@gmail.com>
2025-07-30 12:31:56 -0300

    Melhorias de performance e acessibilidade na página About Me

---
+++ src/app/about-me/head.jsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="preload"
+        as="image"
+        href="/images/about-me/background.webp"
+        type="image/webp"
+      />
+    </>
+  );
+}
